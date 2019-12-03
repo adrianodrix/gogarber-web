a97c7f9c017f22da1e6962e919f41da4f5eee5cc
Adriano Santos <adrianodrix@gmail.com>
2019-12-03 19:52:54 -0300

    first commit

---
+++ src/config/reactotronConfig.js
@@ -0,0 +1,12 @@
+import Reactototron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactototron.configure()
+    .use(reactotronRedux())
+    .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
